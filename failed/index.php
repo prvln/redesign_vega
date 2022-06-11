1b3006253984e0d7580ddfe47502540afbb7880e
Гёля Лис <centavra2012@mail.ru>
2022-06-12 02:05:57 +0300

    неправильный вход

---
+++ failed/index.php
@@ -0,0 +1,19 @@
+<?php
+	require_once('../nutils.php');
+	
+	printHeader('Попробуйте еще раз');
+?>
+
+<div class="container-md mt-5">
+        <h2><strong>Неправильно введен логин или пароль</strong></h2>
+        <p class="lead">Попробуйте еще раз</p>
+        <div style="margin-top : 5%">
+            <h4>Забыли логин или пароль?</h4>
+            <p class="lead">Если забыли логин или пароль обратитесь к системному администратору в Telegram.</p>
+        </div>
+
+    </div>
+	
+<?php
+	printFooter();
+?>
