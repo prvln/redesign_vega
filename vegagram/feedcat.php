a174cd19c134f3189837db483c27025c70e2d3cc
Гёля Лис <centavra2012@mail.ru>
2022-06-13 15:12:18 +0300

    добавлено фото котка

--- vegagram/feedcat.php
+++ vegagram/feedcat.php
@@ -11,6 +11,7 @@
           <p class="lead" align="justify">Дистанционную технологию подкармливания котиков разработали студенты кафедры Борис Лесин и Михаил Ступак. <br><br>С помощью телеграм-бота он командует компьютеру Raspberry Pi и контроллеру на платформе Arduino повернуть банку с кормом с помощью сервомотора, а также сделать фотографию довольного котика.Если на фотографии котик недоволен, можно послать команду подогреть ему коврик.<br><br>Опытный образец эксплуатируется на даче преподавателя кафедры Г. А. Милонова.<br><br></p>
         </div>
         <div class="col-md-4">
+        <img src="../images/other/cat.jpg" class="card-img-top rounded-3" >
           <img src="../images/other/cat1.jpg" class="card-img-top rounded-3" >
           <img src="../images/other/cat3.jpg" class="card-img-top rounded-3" >
           <img src="../images/other/cat2.jpg" class="card-img-top rounded-3" >
